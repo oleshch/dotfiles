31036332295c6848eefb2dfda1afc869f789b790
Orest Leshchyshen <orest.lesh@gmail.com>
2019-10-01 11:27:15 -0400

    First commit

---
+++ brew.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed
+# Install a modern version of Bash.
+brew install bash
+brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
+fi;
+
+# Install more recent versions of some macOS tools.
+brew install grep
+brew install openssh
+brew install screen
+brew install php
+
+
+# Install other useful binaries.
+brew install ack
+brew install git
+brew install git-lfs
+brew install gs
+brew install imagemagick
+brew install rename
+brew install ssh-copy-id
+
+# Install Cask Apps
+brew cask install google-chrome
+brew cask install sublime-text
+brew cask install iterm2
+brew cask install slack
+brew cask install spotify
+
+# Remove outdated versions from the cellar.
+brew cleanup
